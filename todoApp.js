07ffe82e301cfd9c718fbb75be0f2549682a0828
dheerendrakumar63 <dheerendrakumar639292@gmail.com>
2026-07-06 18:26:39 +0530

    Add files via upload

---
+++ todoApp.js
@@ -0,0 +1,34 @@
+let todo = [];
+
+let req = prompt(" enter a your request ");
+  
+
+while(true) {
+    if(req == "quit") {
+        console.log("quitting app");
+        break;
+    }
+
+    if(req == "list") {
+        console.log("-----------");
+       for(let i=0; i<todo.length; i++){
+        console.log(i, todo[i]);
+       } 
+        for(task of todo) {
+            console.log(task);
+        }
+        console.log("-----------");
+    }else if(req == "add"){
+        let task=prompt("please enter the task you want to add");
+        todo.push(task);
+        console.log("task added");
+    }else if(req == "delete"){
+        let idx = prompt("please enter the task index");
+        todo.splice(idx,1);
+        console.log("deleted task");
+    }else{
+        console.log("wrong request");
+    }
+
+    req = prompt("please enter your request");
+}
